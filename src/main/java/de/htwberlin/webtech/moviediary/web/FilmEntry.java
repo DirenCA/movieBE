4fcce4a051822c5373c9e730fbc8edf130cb530d
DirenCA <direncan_akkaya@hotmail.de>
2024-05-01 14:08:21 +0200

    trying to implement TMDB API

--- src/main/java/de/htwberlin/webtech/moviediary/web/FilmEntry.java
+++ src/main/java/de/htwberlin/webtech/moviediary/web/FilmEntry.java
@@ -1,27 +1,46 @@
 package de.htwberlin.webtech.moviediary.web;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-public class FilmEntry{
-    private String title;
-    private String genre;
-    private boolean watched;
+public class FilmEntry {
 
-    public FilmEntry(String title,String genre,boolean watched){
-        this.title=title;
-        this.genre=genre;
-        this.watched=watched;
+    private static final String API_KEY = System.getenv("MOVIE_DB_API_KEY");
+    private HttpClient client;
+
+    public FilmEntry() {
+        this.client = HttpClient.newHttpClient();
     }
 
-    public String getTitle(){
-        return title;
+    public List<String> searchFilmsByQuery(String query) throws IOException, InterruptedException {
+        String encodedQuery = URLEncoder.encode(query, StandardCharsets.UTF_8);
+        String url = "https://api.themoviedb.org/3/search/movie?api_key=" + API_KEY + "&query=" + encodedQuery;
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() != 200) {
+            throw new IOException("Failed to get response from the movie database API");
+        }
+
+        JSONObject jsonResponse = new JSONObject(response.body());
+        JSONArray results = jsonResponse.getJSONArray("results");
+
+        return results.toList().stream()
+                .map(item -> ((JSONObject)item).getString("title"))
+                .collect(Collectors.toList());
     }
-
-    public String getGenre(){
-        return genre;
-    }
-
-    public boolean watched(){return watched; }
-
 }
-
-//
--- src/main/java/de/htwberlin/webtech/moviediary/web/MyController.java
+++ src/main/java/de/htwberlin/webtech/moviediary/web/MyController.java
@@ -1,21 +1,23 @@
 package de.htwberlin.webtech.moviediary.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import org.json.JSONObject;
 
-// Define the class as a RestController
 @RestController
 public class MyController {
 
+    private FilmEntry filmEntry = new FilmEntry();
+
     @GetMapping("/films")
-    public List<FilmEntry> getFilms() {
-        // Create several FilmEntry objects
-        FilmEntry entry = new FilmEntry("Leon", "Action", true);
-        FilmEntry entry1 = new FilmEntry("Der Pate", "Gangster", false);
-        FilmEntry entry2 = new FilmEntry("Avengers", "Superhero", true);
-        //FilmEntry entry3 = new FilmEntry("Avatar", "SciFi", true);
-        // Return a list of these objects
-        return List.of(entry, entry1, entry2);
+    public List<String> searchFilms(@RequestParam String query) {
+        try {
+            return filmEntry.searchFilmsByQuery(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return List.of("Fehler bei der Suche nach Filmen: " + e.getMessage());
+        }
     }
 }
