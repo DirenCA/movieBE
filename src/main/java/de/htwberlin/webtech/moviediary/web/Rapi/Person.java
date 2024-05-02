135547ff2d1ece248372bab1e89f106f7469e686
DirenCA <direncan_akkaya@hotmail.de>
2024-05-02 19:22:24 +0200

    adding Person class for future users

---
+++ src/main/java/de/htwberlin/webtech/moviediary/web/Rapi/Person.java
@@ -0,0 +1,38 @@
+package de.htwberlin.webtech.moviediary.web.Rapi;
+
+public class Person {
+
+    private long id;
+    private String userName;
+    private long password;
+
+    public Person(long id, long password, String userName) {
+        this.id = id;
+        this.password = password;
+        this.userName = userName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public long getPassword() {
+        return password;
+    }
+
+    public void setPassword(long password) {
+        this.password = password;
+    }
+}
