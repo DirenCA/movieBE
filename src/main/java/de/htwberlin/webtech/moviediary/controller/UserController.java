02629e9ae9263b977f7d816d743d3682532fb390
DirenCA <direncan_akkaya@hotmail.de>
2024-06-17 00:20:24 +0200

    error handling for wrong username in login2

--- src/main/java/de/htwberlin/webtech/moviediary/controller/UserController.java
+++ src/main/java/de/htwberlin/webtech/moviediary/controller/UserController.java
@@ -1,11 +1,11 @@
 package de.htwberlin.webtech.moviediary.controller;
 
-import de.htwberlin.webtech.moviediary.model.FilmEntry;
 import de.htwberlin.webtech.moviediary.model.FilmUser;
 import de.htwberlin.webtech.moviediary.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3003")
@@ -32,10 +32,23 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<FilmUser> loginUser(@RequestBody FilmUser filmUser) {
+    public ResponseEntity<FilmUser> loginUser(@RequestBody FilmUser filmUser, HttpSession httpSession) {
         FilmUser user = userService.loginUser(filmUser.getUserName(), filmUser.getPassword());
+        httpSession.setAttribute("loggedInUser", user);
         return ResponseEntity.ok(user);
     }
+
+    @GetMapping("/isloggedin")
+    public ResponseEntity<Boolean> isLoggedIn(HttpSession httpSession) {
+        return ResponseEntity.ok(httpSession.getAttribute("loggedInUser") != null);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logoutUser(HttpSession httpSession) {
+        httpSession.invalidate();
+        return ResponseEntity.ok().build();
+    }
+
 
 }
 
