a088b1bcadcb884f287d4a53a82114726f879f5a
DirenCA <direncan_akkaya@hotmail.de>
2024-06-25 22:30:43 +0200

    render deployment: changed webconfig  -> edit webconfig

--- src/main/java/de/htwberlin/webtech/moviediary/WebConfig.java
+++ src/main/java/de/htwberlin/webtech/moviediary/WebConfig.java
@@ -3,9 +3,11 @@ package de.htwberlin.webtech.moviediary;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
