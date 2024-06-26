ab22d8477846957baca238a571e0bbc6cb3f483d
DirenCA <direncan_akkaya@hotmail.de>
2024-06-26 21:39:16 +0200

    added git testing

--- src/test/java/de/htwberlin/webtech/moviediary/MoviediaryApplicationTests.java
+++ src/test/java/de/htwberlin/webtech/moviediary/MoviediaryApplicationTests.java
@@ -1,13 +1,41 @@
 package de.htwberlin.webtech.moviediary;
 
+import de.htwberlin.webtech.moviediary.controller.UserController;
+import de.htwberlin.webtech.moviediary.model.FilmUser;
+import de.htwberlin.webtech.moviediary.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class MoviediaryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@MockBean
+	private UserService userService;
 
+	@Autowired
+	private UserController userController;
+
+	@Test
+	void testRegisterUser() {
+		String userName = "Test User";
+		String password = "Test Password";
+
+		FilmUser mockUser = new FilmUser();
+		mockUser.setUserName(userName);
+		mockUser.setPassword(password);
+
+		when(userService.registerUser(userName, password)).thenReturn(mockUser);
+
+		ResponseEntity<FilmUser> response = userController.registerUser(mockUser);
+		FilmUser result = response.getBody();
+
+		assertEquals(mockUser, result);
+		assertEquals(200, response.getStatusCodeValue());
+	}
 }
